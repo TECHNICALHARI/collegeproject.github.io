90d12f55736972ddcb37eaeb00742620ce0a391f
TECHNICALHARI <pandayhariom2898@gmail.com>
2020-05-17 12:25:01 -0700

    college project website

---
+++ main.js
@@ -0,0 +1,18 @@
+jquery(document).ready(function(){
+
+    "use sctrict"
+     $('slider').ripples({
+        dropRadius: 15,
+        perturbance: 0.01,
+        
+      });
+      $(".text").typed({
+        strings:("first sentence.","second sentence."),
+        typespeed:0,
+        loop:true
+      });
+})
+
+// 
+font-family: 'Faster One', cursive; for brand
+font-family: 'Pacifico', cursive; for brand
